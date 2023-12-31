083fd9ce5bb17c4b98922210f2f91f94e1b7ffcf
Yz-btw <155298096+Yz-btw@users.noreply.github.com>
2023-12-31 17:12:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const handleClickViewMore = (element) => {
+    if(element.classList.contains("opened")){
+      element.classList.remove("opened")
+    }else{
+      element.classList.add("opened")
+    }
+  }
